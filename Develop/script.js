8211e1ed8db99fd4f2ff07e2ba56da3ee0d50cbf
James Hughes <jameshughes2009@hotmail.com>
2024-02-10 01:51:26 -0500

    I lied more work on JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,12 @@
+var cityInput = document.querySelector(".city-input")
 var searchButton = document.querySelector(".search-btn")
+
+
+var getCityInfo =() => {
+    var cityName = cityInput.value.trim(); // Get User to enter city name
+    if(!cityName) return; // return if city name empty
+
+    console.log(cityName)
+}
 
 searchButton.addEventListener("click", getCityInfo)
