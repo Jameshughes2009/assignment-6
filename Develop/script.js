a58ff1ca51669bab4f3499d3a5cc944df09a534f
James Hughes <jameshughes2009@hotmail.com>
2024-02-10 02:59:45 -0500

    api will only give error message

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,20 @@
 var cityInput = document.querySelector(".city-input")
 var searchButton = document.querySelector(".search-btn")
 
+var API_KEY = "608bd88476d2913911a7aa1be9fe2978" //My API key
+
 
 var getCityInfo =() => {
     var cityName = cityInput.value.trim(); // Get User to enter city name
     if(!cityName) return; // return if city name empty
+    var GEOCODING_API_URL = 'http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${API_KEY}'
+    // console.log(cityName)
 
-    console.log(cityName)
+    fetch(GEOCODING_API_URL).then(res => res.json()).then(data => {
+        console.log(data)
+    }).catch(() => {
+        alert("Dezz Nu")
+    })
 }
 
 searchButton.addEventListener("click", getCityInfo)
