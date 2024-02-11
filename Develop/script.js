d7c97d5dc84868d67ec0d77c13f9625471784e10
James Hughes <jameshughes2009@hotmail.com>
2024-02-11 08:53:52 -0500

    preparing for turtoring Session

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,8 @@
 var cityInput = document.querySelector(".city-input")
 var searchButton = document.querySelector(".search-btn")
+var locationButton = document.querySelector(".location-btn")
+var currentWeatherDiv = document.querySelector(".current-weather")
+var weatherCardsDiv = document.querySelector("weather-cards")
 
 var API_KEY = "2511bbd53378a380481fc0227749b46b" //My API key
 
